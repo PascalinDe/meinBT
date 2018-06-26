2465e6b48c6fb001cf1498d088e1b8eacd6bad52
Carine Dengler <git@carine-dengler.de>
2018-06-26 15:44:55 +0200

    mbtxml.py
    - cleaned license notice up

--- src/mbtxml.py
+++ src/mbtxml.py
@@ -1,7 +1,7 @@
-#    meinBT
+#    This file is part of meinBT 1.0.
 #    Copyright (C) 2017  Carine Dengler
 #
-#    This program is free software: you can redistribute it and/or modify
+#    meinBT is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
